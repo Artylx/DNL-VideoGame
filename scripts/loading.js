d34dfc57c03e96d38a9e492d0b600cb094c930c5
PC-Fixe <ptitg@DESKTOP-O40PV9D>
2025-12-18 21:33:00 +0100

    loading.js

---
+++ scripts/loading.js
@@ -0,0 +1,24 @@
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "/DNL-VideoGame/styles/loading.css";
+
+window.addEventListener("load", () => {
+    const overlay = document.createElement("div");
+    document.body.appendChild(div);
+    div.id = "loading-overlay";
+
+    div.innerHTML = '<img src="/DNL-VideoGame/assets/logo_dnl.png" alt="Chargement" class="loading-logo">'
+ 
+    const MIN_DURATION = 1000;
+    const start = performance.now();
+
+    const hide = () => {
+        overlay.classList.add("hide");
+        setTimeout(() => overlay.remove(), 300);
+    };
+
+    const elapsed = performance.now() - start;
+    const remaining = Math.max(0, MIN_DURATION - elapsed);
+
+    setTimeout(hide, remaining);
+});
